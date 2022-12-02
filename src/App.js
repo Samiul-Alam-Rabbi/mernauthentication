1469201d20f89b29ec7ca08dfa80db791181a8b5
msar3469 <msar3469@gmail.com>
2022-12-02 09:54:19 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Mailer from './components/Mailer';
+// import ForgetPassword from './components/ForgetPassword';
+import NewSubmit from './components/NewSubmit';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/forget" element={<Mailer/>} />
+      {/* <Route path="/forget" element={<ForgetPassword/>} /> */}
+      <Route path="/otp" element={<NewSubmit/>} />
+    </Routes>
+  </Router>
 }
 
 export default App;
